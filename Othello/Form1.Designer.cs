7f4828f2a60da70b043bf98322687f566b27c355
Sangjin Lee <lsj_work@naver.com>
2018-09-11 09:47:40 +0900

    Othello Ver. 1.0

---
+++ Othello/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Othello
+{
+    partial class Form_Othello
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.TurnBox = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.TurnBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TurnBox
+            // 
+            this.TurnBox.AutoSize = true;
+            this.TurnBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.TurnBox.Controls.Add(this.label2);
+            this.TurnBox.Controls.Add(this.button1);
+            this.TurnBox.Controls.Add(this.pictureBox2);
+            this.TurnBox.Controls.Add(this.label1);
+            this.TurnBox.Controls.Add(this.pictureBox1);
+            this.TurnBox.Font = new System.Drawing.Font("나눔고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TurnBox.Location = new System.Drawing.Point(608, 46);
+            this.TurnBox.Name = "TurnBox";
+            this.TurnBox.Size = new System.Drawing.Size(104, 140);
+            this.TurnBox.TabIndex = 1;
+            this.TurnBox.TabStop = false;
+            this.TurnBox.Text = "TURN";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Othello.Properties.Resources.TrunBlack;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BLACK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "WHITE";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Othello.Properties.Resources.TrunWhite;
+            this.pictureBox2.Location = new System.Drawing.Point(10, 61);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // Form_Othello
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 613);
+            this.Controls.Add(this.TurnBox);
+            this.Name = "Form_Othello";
+            this.Text = "오셀로";
+            this.TurnBox.ResumeLayout(false);
+            this.TurnBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox TurnBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Othello/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Othello
+{
+    public partial class Form_Othello : Form
+    {
+
+        public Board Othello_Board = new Board();
+
+        public Form_Othello()
+        {
+            InitializeComponent();
+            Controls.Add(Othello_Board.Panel_Board);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Othello_Board.Reset();
+        }
+    }
+
+    public class Board
+    {
+        public Piece[,] pieces = new Piece[8, 8];
+        public Panel Panel_Board = new Panel();
+        public int Turn = 1;
+        public int possible_num = 4;
+        bool is_Passed = false;
+
+        public Board()
+        {
+            Panel_Board.AutoScroll = true;
+            Panel_Board.AutoSize = true;
+            Panel_Board.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            Panel_Board.BorderStyle = BorderStyle.FixedSingle;
+            Panel_Board.Location = new Point(3, 3);
+            Panel_Board.Name = "Panel_Board";
+            Panel_Board.Size = new Size(2, 2);
+            Panel_Board.TabIndex = 0;
+
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    Piece a_Piece = new Piece();
+                    Panel_Board.Controls.Add(a_Piece.Piece_Image);
+                    a_Piece.Piece_Image.Name = "Piece_Box_" + x.ToString() + "_" + y.ToString();
+                    if ((x == 3 && y == 3) || (x == 4 && y == 4))
+                        a_Piece.Set_Status(Piece.Status.black);
+                    else if ((x == 4 && y == 3) || (x == 3 && y == 4))
+                        a_Piece.Set_Status(Piece.Status.white);
+                    else
+                        a_Piece.Set_Status(Piece.Status.empty);
+                    a_Piece.Set_Position(x, y);
+                    a_Piece.Piece_Image.Click += new EventHandler(Piece_Click);
+                    pieces[x, y] = a_Piece;
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    Piece a_Piece = pieces[x, y];
+                    if ((x == 3 && y == 3) || (x == 4 && y == 4))
+                        a_Piece.Set_Status(Piece.Status.black);
+                    else if ((x == 4 && y == 3) || (x == 3 && y == 4))
+                        a_Piece.Set_Status(Piece.Status.white);
+                    else
+                        a_Piece.Set_Status(Piece.Status.empty);
+                }
+            }
+
+            Turn = 1;
+            possible_num = 4;
+            is_Passed = false;
+    }
+
+        public void Piece_Click(object sender, EventArgs e)
+        {
+            PictureBox a_Piece_Image = (PictureBox)sender;
+            string[] str = a_Piece_Image.Name.Split('_');
+            int x = Convert.ToInt32(str[2]);
+            int y = Convert.ToInt32(str[3]);
+
+            Scan(x, y, 0);
+
+            if (Turn == 1 && pieces[x, y].isPossible) 
+            {
+                Reburse();
+                Turn = 2;
+            }
+            else if (Turn == 2 && pieces[x, y].isPossible)
+            {
+                Reburse();
+                Turn = 1;
+            }
+
+            Scan_all();
+            if (Turn == 0) ;
+            else{
+                for (int j = 0; j < 8; j++)
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        pieces[i, j].isPossible = false;
+                    }
+                }
+            }
+
+        }
+
+        public void Reburse()
+        {
+            Piece.Status stat = new Piece.Status();
+            if (Turn == 1) stat = Piece.Status.black;
+            else if (Turn == 2) stat = Piece.Status.white;
+
+            for (int y = 0; y < 8; y++)
+            {
+                for(int x = 0; x < 8; x++)
+                {
+                    if (pieces[x, y].isPossible)
+                    {
+                        pieces[x, y].Set_Status(stat);
+                        pieces[x, y].isPossible = false;
+                    }
+                }
+            }
+        }
+
+        public void Scan_all()
+        {
+            possible_num = 0;
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    Scan(x, y,0);
+                    if (pieces[x, y].isPossible) possible_num++;
+                }
+            }
+            if (possible_num == 0)
+            {
+                if (is_Passed)
+                {
+                    Turn = 0;
+                    is_Passed = false;
+                }
+                else
+                {
+                    if (Turn == 1) Turn = 2;
+                    else if (Turn == 2) Turn = 1;
+                    is_Passed = true;
+                    Scan_all();
+                }
+            }
+        }
+
+        public void Scan(int x, int y, int index)
+        {
+            pieces[x, y].isPossible = false;
+
+            Piece.Status stat = new Piece.Status();
+            if (Turn == 1) stat = Piece.Status.white;
+            else if (Turn == 2) stat = Piece.Status.black;
+
+            Piece[] piece_Array = new Piece[9];
+            for(int i = 0; i < 9; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        piece_Array[i] = pieces[x, y];
+                        break;
+                    case 1:
+                        if (x == 0 || y == 0) piece_Array[i] = null;
+                        else piece_Array[i] = pieces[x - 1, y - 1];
+                        break;
+                    case 2:
+                        if (y == 0) piece_Array[i] = null;
+                        else piece_Array[i] = pieces[x, y - 1];
+                        break;
+                    case 3:
+                        if (x == 7 || y == 0) piece_Array[i] = null;
+                        else piece_Array[i] = pieces[x + 1, y - 1];
+                        break;
+                    case 4:
+                        if (x == 0) piece_Array[i] = null;
+                        else piece_Array[i] = pieces[x - 1, y];
+                        break;
+                    case 5:
+                        if (x == 7) piece_Array[i] = null;
+                        else piece_Array[i] = pieces[x + 1, y];
+                        break;
+                    case 6:
+                        if (x == 0 || y == 7) piece_Array[i] = null;
+                        else piece_Array[i] = pieces[x - 1, y + 1];
+                        break;
+                    case 7:
+                        if (y == 7) piece_Array[i] = null;
+                        else piece_Array[i] = pieces[x, y + 1];
+                        break;
+                    case 8:
+                        if (x == 7 || y == 7) piece_Array[i] = null;
+                        else piece_Array[i] = pieces[x + 1, y + 1];
+                        break;
+                }
+            }
+            if (index == 0)
+            {
+                if (piece_Array[0].status == Piece.Status.empty)
+                {
+                    for (int j = 1; j < 9; j++)
+                    {
+                        if (piece_Array[j] != null)
+                        {
+                            if (piece_Array[j].status == stat)
+                            {
+                                switch (j)
+                                {
+                                    case 1: Scan(x - 1, y - 1, j); break;
+                                    case 2: Scan(x, y - 1, j); break;
+                                    case 3: Scan(x + 1, y - 1, j); break;
+                                    case 4: Scan(x - 1, y, j); break;
+                                    case 5: Scan(x + 1, y, j); break;
+                                    case 6: Scan(x - 1, y + 1, j); break;
+                                    case 7: Scan(x, y + 1, j); break;
+                                    case 8: Scan(x + 1, y + 1, j); break;
+                                }
+                            }
+                            else piece_Array[j].isPossible = false;
+                            piece_Array[0].isPossible |= piece_Array[j].isPossible;
+                        }
+                    }
+                }
+                else piece_Array[0].isPossible = false;
+            }
+            else
+            {
+                if (piece_Array[index] != null)
+                {
+                    if (piece_Array[index].status == stat)
+                    {
+                        switch (index)
+                        {
+                            case 1: Scan(x - 1, y - 1, index); break;
+                            case 2: Scan(x, y - 1, index); break;
+                            case 3: Scan(x + 1, y - 1, index); break;
+                            case 4: Scan(x - 1, y, index); break;
+                            case 5: Scan(x + 1, y, index); break;
+                            case 6: Scan(x - 1, y + 1, index); break;
+                            case 7: Scan(x, y + 1, index); break;
+                            case 8: Scan(x + 1, y + 1, index); break;
+                        }
+                    }
+                    else if (piece_Array[index].status == Piece.Status.empty)
+                    {
+                        piece_Array[0].isPossible = false;
+                        piece_Array[index].isPossible = false;
+                    }
+                    else  piece_Array[index].isPossible = true;
+                    piece_Array[0].isPossible |= piece_Array[index].isPossible;
+                }
+                else piece_Array[0].isPossible = false;
+            }
+        }
+    }
+
+    public class Piece
+    {
+        public PictureBox Piece_Image;
+        public Status status;
+        public Point position;
+        public bool isPossible = false;
+
+        public Piece()
+        {
+            Piece_Image = new PictureBox();
+            Piece_Image.Size = new Size(70, 70);
+            Piece_Image.Margin = new Padding(1);
+            Piece_Image.SizeMode = PictureBoxSizeMode.StretchImage;
+            status = Status.empty;
+            position = new Point(0, 0);
+            Set_Status(status);
+        }
+        
+        public void Set_Status(Status stat)
+        {
+            status = stat;
+            switch (stat)
+            {
+                case Status.black:
+                    Piece_Image.Image = Properties.Resources.BlackPiece;
+                    break;
+                case Status.white:
+                    Piece_Image.Image = Properties.Resources.WhitePiece;
+                    break;
+                case Status.empty:
+                    Piece_Image.Image = Properties.Resources.Empty;
+                    break;
+            }
+        }
+        
+        public void Set_Position(int x, int y)
+        {
+            position = new Point(x,y);
+            Piece_Image.Location = new Point(1 + x * 70, 1 + y * 70);
+        }
+        
+        public enum Status
+        {
+            empty = 2,
+            black = 0,
+            white = 1
+        }
+    }
+
+}
